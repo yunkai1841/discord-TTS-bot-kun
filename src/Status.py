df8a22be3bce6f8cdfb539c36c3a9239db963597
yunkai1841 <70383706+yunkai1841@users.noreply.github.com>
2021-07-07 20:30:56 +0900

    create Status function

--- src/Status.py
+++ src/Status.py
@@ -1,4 +1,13 @@
 class Status:
-    Ready = False
 
-    def __init__():
+    def __init__(self):
+        self.__ready = False
+        print(self.__class__, "Ready")
+
+    # check server status
+    def is_ready(self):
+        return self.__ready
+
+    def set_ready(self, ready):
+        self.__ready = ready
+
