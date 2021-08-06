8707a27e02361d87816014061509f3208e888a4f
yunkai1841 <70383706+yunkai1841@users.noreply.github.com>
2021-08-06 19:52:20 +0900

    fix path

--- src/settings.py
+++ src/settings.py
@@ -3,7 +3,7 @@ import json
 import status
 
 def get_token(debug=False):
-    with open(file=r"setting/token.json") as f:
+    with open(file=r"settings/token.json") as f:
         tokens = json.load(f)
         
         if debug:
