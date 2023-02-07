d7ab5bc0b8c0545cda649506c6d0adc36731068b
yunkai1841 <70383706+yunkai1841@users.noreply.github.com>
2023-02-08 01:25:13 +0900

    implement text_to_speech

--- src/speek.py
+++ src/speek.py
@@ -1,86 +1,43 @@
-from google.cloud import texttospeech
-import re
+import requests
+import json
+import time
 
-tts_client = texttospeech.TextToSpeechClient()
+base_url = "http://localhost:50021"
 
-def text_to_ssml(txt: str) -> str:
-    """Str to ssml (beta)
-    See ssml reference at  
-    https://cloud.google.com/text-to-speech/docs/ssml
-    https://www.w3.org/TR/speech-synthesis/
 
-     - escape url
-     - braek when encounter "\\n"
-    """
+def synthesis(text: str, filename: str, speaker: int = 1, max_retry: int = 20, debug: bool = False):
+    # speeker = 1: ずんだもん
 
-    #replace URL(spell-out)
-    url_pattern = "https?://[\w/:%#\$&\?\(\)~\.=\+\-]+"
-    txt = re.sub(url_pattern, "<say-as interpret-as=\"verbatim\">URL</say-as>", txt)
+    # audio_query
+    query_payload = {"text": text, "speaker": speaker}
+    for query_i in range(max_retry):
+        r = requests.post(base_url + "/audio_query",
+                          params=query_payload, timeout=(10.0, 300.0))
+        if r.status_code == 200:
+            query_data = r.json()
+            break
+        time.sleep(1)
+    else:
+        raise ConnectionError(
+            "リトライ回数が上限に到達しました。 audio_query : ", filename, "/", text[:30], r.text)
 
-    #replace \n to break time
-    txt.replace("\n", "\n<break time=\"1s\"/>")
+    # synthesis
+    synth_payload = {"speaker": speaker}
+    for synth_i in range(max_retry):
+        r = requests.post(base_url + "/synthesis", params=synth_payload,
+                          data=json.dumps(query_data), timeout=(10.0, 300.0))
+        if r.status_code == 200:
+            with open(filename, "wb") as fp:
+                fp.write(r.content)
+            if debug:
+                print(
+                    f"{filename} は query={query_i+1}回, synthesis={synth_i+1}回のリトライで正常に保存されました")
+            break
+        time.sleep(1)
+    else:
+        raise ConnectionError("リトライ回数が上限に到達しました。 synthesis : ",
+                              filename, "/", text[:30], r, text)
 
-    return txt
 
-def ssml_to_speech(
-    ssml: str, 
-    language = "ja_JP", 
-    voice_name = "ja-JP-Standard-A", 
-    gender = None,
-    outputfile = None):
-    """Pass text to gcloud
-    see reference
-    https://cloud.google.com/text-to-speech/docs/reference/rpc/google.cloud.texttospeech.v1
-
-    Return:
-        audio content(binary)
-    """
-    input_text = texttospeech.SynthesisInput(ssml=ssml)
-
-    voice = texttospeech.VoiceSelectionParams(
-        language_code = language,
-        name=voice_name,
-        # ssml_gender=texttospeech.SsmlVoiceGender.SSML_VOICE_GENDER_UNSPECIFIED,
-    )
-    #TODO speaking_rate, pitch, valume_gain_db
-    audio_config = texttospeech.AudioConfig(
-        audio_encoding=texttospeech.AudioEncoding.LINEAR16,
-        sample_rate_hertz=96000
-    )
-    response = tts_client.synthesize_speech(
-        input=input_text, voice=voice, audio_config=audio_config
-    )
-
-    if outputfile != None:
-        with open(outputfile, "wb") as out:
-            out.write(response.audio_content)
-            print(f'Audio content written to file {outputfile}')
-
-    return response.audio_content
-
-def list_voices(language: str = None, debug: bool = False) -> list:
-    """Get list of availiable voices
-    see reference
-    https://cloud.google.com/text-to-speech/docs/reference/rpc/google.cloud.texttospeech.v1
-
-    Param:
-        language: language_code (default=None)
-        see website
-        http://www.lingoes.net/en/translator/langcode.htm
-
-    example:
-        list_voices("ja_JP")
-    """
-    voices = tts_client.list_voices(language_code=language)
-
-    voice_names = []
-    for voice in voices.voices:
-        voice_names.append(voice.name)
-
-    if debug:
-        for voice in voices.voices:
-            print(f"Name: {voice.name}")
-            gender = texttospeech.SsmlVoiceGender(voice.ssml_gender)
-            print(f"Gender: {gender.name}")
-
-    return voice_names
+def text_to_speech(text: str, filename: str, speaker: int = 1):
+    synthesis(text, filename, speaker)
